507a0ad87c99233659851fe5d37325f93c31083c
Mbafraude <lopez.martin.oscar.giner@gmail.com>
2025-12-09 13:28:38 +0100

    Update init_db.py

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Añadir el directorio actual al path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.init import create_app, db
 from app.models import User, Game
 
